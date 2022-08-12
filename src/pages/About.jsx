15a777745f7846a2924287f2380f443622cecaa9
khumbulani <kerkhood9@gmail.com>
2022-08-12 10:10:48 +0200

    added about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,9 +5,9 @@ function About() {
   return (
     <div>
       <Header name="About" />
-      <p> I am your as the should ticket free .
-  <p>They know people like really .</p> 
-    Movie in 2022 </p>
+      <p> I am your as they should  buy ticket
+        they know people like really the film  have will need by can did mean everyone one in this great maybe feel night movie life</p>
+        <h1>Movie in 2022 </h1>
    
     </div>
   );
