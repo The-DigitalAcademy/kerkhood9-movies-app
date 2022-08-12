6f4076106aeb31a4cf08f7956d1c25e2ac947e5a
khumbulani <kerkhood9@gmail.com>
2022-08-12 10:15:50 +0200

    added new file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,8 +7,10 @@ function About() {
       <Header name="About" />
       <p> I am your as they should  buy ticket
         they know people like really the film  have will need by can did mean everyone one in this great maybe feel night movie life</p>
-        <h1>Movie in 2022 </h1>
+        <h1>Movie in 2022</h1>
    
+
+
     </div>
   );
 }
