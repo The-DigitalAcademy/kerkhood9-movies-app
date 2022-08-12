7297fe7f4ba6e7ac6a7f955fba8bcc19fb218577
khumbulani <kerkhood9@gmail.com>
2022-08-12 10:48:28 +0200

    added home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,35 +23,35 @@ function Home() {
           <Card
             image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpenZ7mfDd3JcGBZGT1rS-BRblyfXBOMJE0w&usqp=CAU'
             name='Bease'
-            price='R1600'
+            
           />
 
           <Card
             image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa8p5ldazsuz83WNzWY_HUnCdJxgvpViP3fQ&usqp=CAU'
             name='Brahastra'
-            price='R100'
+            
           />
 
           <Card
             image='https://cdn.moviefone.com/admin-uploads/posters/morbius-movie-poster_1638710998.jpg?d=360x540&q=50'
             name='Morbius'
-            price='R1000'
+           
           />
           <Card
             image='https://cdn.moviefone.com/admin-uploads/posters/super-pets-movie-poster_1656124357.jpg?d=360x540&q=50' 
             name='Superpets'
-            price='R500'
+            
           />
           <Card
             image='https://cdn.vox-cdn.com/thumbor/l45XEEqbJXzQf6LTnTT_pVZzg1M=/1400x0/filters:no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/22488068/image_2021_05_03_090558.png'
             name='Wakanda forever'
-            price='R450'
+            
           />
 
           <Card
             image='https://m.media-amazon.com/images/M/MV5BZTZhYzk1ZDYtMmMwMS00YzhlLWI0MmEtZWI1ZjU1YTdlZmJmXkEyXkFqcGdeQXVyMzYzNzc1NjY@._V1_.jpg'
             name='street of fight'
-            price='R12500'
+            
           />
         </div>
       </div>
