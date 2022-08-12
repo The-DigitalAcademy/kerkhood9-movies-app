da0c0085e46033cc933a852cdf419881b703750e
khumbulani <kerkhood9@gmail.com>
2022-08-12 13:28:54 +0200

    added home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,13 +11,13 @@ function Home() {
           <Card 
             image='https://www.thebeyondnews.com/assets/admin/images/postimage/The-beyond-news-Doctor%20Strange%20in%20the%20Multiverse%20of%20Madness%20Movie%202022-%20release%20date,%20cast,%20story,%20teaser,%20trailer,%20first%20look,%20rating,%20reviews,%20box%20office%20collection%20and%20preview.jpg'
             name='Doctor Strange Multiverse of Madness'
-            price='R500'
+            
           />
 
           <Card
             image='https://sportshub.cbsistatic.com/i/2022/04/18/ccb71c63-2cf3-44dd-8078-0846f97a874c/thor-love-and-thunder-poster.jpg?auto=webp&width=864&height=1280&crop=0.675:1,smart'
             name='Thor Love and thunder'
-            price='R4000'
+            
           />
 
           <Card
