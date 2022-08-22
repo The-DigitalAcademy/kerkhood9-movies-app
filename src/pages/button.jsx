ec18ac805ad5980894ef6e0cdecdd4a3ef0e1b10
khumbulani <kerkhood9@gmail.com>
2022-08-22 08:11:50 +0200

    added name

---
+++ src/pages/button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import button from 'button'
+
+function button() {
+  return (
+    <div>
+        <div className="button">submit</div>
+    </div>
+  )
+}
+
+export default button
